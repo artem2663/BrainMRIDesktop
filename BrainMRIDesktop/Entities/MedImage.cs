102f35530e65e792362672bf830dace4b81e6406
artem2663 <artem2663@gmail.com>
2020-11-01 18:24:53 +0200

    entities

---
+++ BrainMRIDesktop/Entities/MedImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BrainMRIDesktop
+{
+    public class MedImage
+    {
+        public DateTime CreationDate { get; private set; }
+        public string Interval { get; private set; }
+        public string ImgType { get; private set; }
+        public string Age { get; private set; }
+        public string Gender { get; private set; }
+
+
+        public MedImage(DateTime creationDate, string interval, string imgType, string age, string gender)
+        {
+            CreationDate = creationDate;
+            Interval = interval;
+            ImgType = imgType;
+            Age = age;
+            Gender = gender;
+        }
+
+    }
+}
---
+++ BrainMRIDesktop/Entities/Patient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainMRIDesktop
+{
+    public class Patient
+    {
+        public int ID { get; private set; }
+        public string MedDate { get; private set; }
+        public int Age { get; private set; }
+        public string Gender { get; private set; }
+        public string Diagnosis { get; private set; }
+        public int IsVerified { get; private set; }
+
+
+        public Patient(int Id, string medDate, int age, string gender, string diagnosis, int isVerified)
+        {
+            ID = Id;
+            MedDate = medDate;
+            Age = age;
+            Gender = gender;
+            Diagnosis = diagnosis;
+            IsVerified = isVerified;
+        }
+    }
+}
